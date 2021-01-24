fe2de6b1991ef21e84792fd982e36858fa22981f
Guillaume ROUSSEAU <guillaume.rousseau@univ-paris-diderot.fr>
2021-01-24 10:48:15 +0100

    Data Update

--- load.py
+++ load.py
@@ -71,6 +71,6 @@ color=['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
               '#9467bd', '#8c564b', '#e377c2', '#7f7f7f',
               '#bcbd22', '#17becf']
 
-data3,fields=ReadClasseAge(dx)
+data3,fields,dfields=ReadClasseAge(dx)
 
 dataN=ReadNouveaux(dx)
